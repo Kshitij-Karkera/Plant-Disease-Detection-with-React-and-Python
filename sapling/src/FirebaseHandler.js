98797338e42defbf61f1457d41c59b8b0e6e2bc9
Kshitij-Karkera <kshitu2308@gmail.com>
2025-01-13 19:01:24 +0530

    Remove sensitive data from FireHandler.js and updated .gitignore

--- sapling/src/FirebaseHandler.js
+++ sapling/src/FirebaseHandler.js
@@ -1,19 +1,18 @@
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
-
 const firebaseConfig = {
-    apiKey: "AIzaSyBk-ny5Hu3vZSDhB62xHcWwPDrFFyxAVuU",
-    authDomain: "sapling-1bf9f.firebaseapp.com",
-    projectId: "sapling-1bf9f",
-    storageBucket: "sapling-1bf9f.appspot.com",
-    messagingSenderId: "494773261290",
-    appId: "1:494773261290:web:0828d6a8c0e56d1ea75893",
-    measurementId: "G-XCLQ90LDZ3"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
 
 const storage = getStorage(app);
 
-export { storage }
+export { storage };
